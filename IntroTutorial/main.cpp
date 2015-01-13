3be794fed7c81015adf6759c79f6017403cd7c47
CK Huang <kertmanster@gmail.com>
2015-01-13 13:42:59 +0800

    Initial Commit

---
+++ IntroTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IntroTutorial
+//
+//  Created by CKHuang on 1/13/15.
+//  Copyright (c) 2015 CKHuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
